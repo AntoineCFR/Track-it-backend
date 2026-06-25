0b2ee34e9fb041ecf8588ad15cdbf75f1ba045e8
unknown <antoine.cosne@ik.me>
2026-06-25 08:46:56 +0200

    First commit. Testing Hello World on a Flask server using Render.

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import os
+from firestore_API.firestore_helpers import firestore_main
+
+def main():
+    #firestore_secret = os.environ.get('FIRESTORE_SECRET')
+    #firestore_main(firestore_secret)
+    return 1
+
+if __name__ == "__main__":
+    main()
