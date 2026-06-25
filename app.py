4baef2f2705015ff03ee595a9102bfbac58f47d5
unknown <antoine.cosne@ik.me>
2026-06-25 13:53:38 +0200

    Trying to fix issue in jsonifying Firestore collection.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.StreamHandler()])
 
-print(f'API Key starting with {Config.FIRESTORE_API_KEY[10]} retrieved from environment variables')
+print(f'API Key {Config.FIRESTORE_API_KEY} retrieved from environment variables')
 Config.firestore_client = initialize_firestore_app(Config.FIRESTORE_API_KEY)
 
 app = Flask(__name__)
