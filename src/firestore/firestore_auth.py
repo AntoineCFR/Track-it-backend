0f91d188d48ae285c7c6f8fadcc486ba9a47dc9a
unknown <antoine.cosne@ik.me>
2026-06-25 13:30:42 +0200

    Trying to fix issue in jsonifying Firestore collection.

--- src/firestore/firestore_auth.py
+++ src/firestore/firestore_auth.py
@@ -1,6 +1,6 @@
 # Standard libraries
 import logging
-import tempfile
+import json
 
 # Third-party libraries
 import firebase_admin
@@ -14,11 +14,14 @@ from firebase_admin import firestore
 logger = logging.getLogger(__name__)
 
 def initialize_firestore_app(firestore_secret):
+
+    if not firestore_secret:
+        logger.error("FIRESTORE_SECRET empty or missing.")
+        raise ValueError(
+            "Could not retrieve FIRESTORE_SECRET from environment variables."
+        )
     logger.info('Attempting to create JSON file for Firebase credentials...')
-    with tempfile.NamedTemporaryFile(mode='w', suffix='.json', delete=False) as ff:
-        ff.write(firestore_secret)
-        firestore_secret_json = ff.name
-    cred = credentials.Certificate(firestore_secret_json)
-    logger.info(f'Trying to authenticate to Firebase using {firestore_secret_json}...')
+    cred = credentials.Certificate(json.load(firestore_secret))
+    logger.info(f'Trying to authenticate to Firebase using JSON...')
     firebase_admin.initialize_app(cred)
     return firestore.client()
